d4198563111659837b37d6a9cd039aecc99f6eb3
Noboru Saito <noborusai@gmail.com>
2019-02-11 19:51:01 +0900

    update

--- writer.go
+++ writer.go
@@ -11,34 +11,32 @@ import (
 
 // Writer is writer struct.
 type Writer struct {
-	Definition
 	Writer io.Writer
 }
 
 // NewWriter is Writer
-func NewWriter(writer io.Writer, definition Definition) *Writer {
+func NewWriter(writer io.Writer) *Writer {
 	return &Writer{
-		Definition: definition,
-		Writer:     writer,
+		Writer: writer,
 	}
 }
 
 // WriteDefinition is write table definition.
-func (tw *Writer) WriteDefinition() error {
-	err := tw.writeComment()
+func (w *Writer) WriteDefinition(d Definition) error {
+	err := w.writeComment(d)
 	if err != nil {
 		return err
 	}
-	err = tw.writeExtra()
+	err = w.writeExtra(d)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (tw *Writer) writeComment() error {
-	for _, comment := range tw.Comments {
-		_, err := io.WriteString(tw.Writer, fmt.Sprintf("# %s\n", comment))
+func (w *Writer) writeComment(d Definition) error {
+	for _, comment := range d.Comments {
+		_, err := io.WriteString(w.Writer, fmt.Sprintf("# %s\n", comment))
 		if err != nil {
 			return err
 		}
@@ -46,26 +44,26 @@ func (tw *Writer) writeComment() error {
 	return nil
 }
 
-func (tw *Writer) writeExtra() error {
-	err := tw.writeExtraWithOutHash()
+func (w *Writer) writeExtra(d Definition) error {
+	err := w.writeExtraWithOutHash(d)
 	if err != nil {
 		return err
 	}
-	return tw.writeExtraWithHash()
+	return w.writeExtraWithHash(d)
 }
 
-func (tw *Writer) writeExtraWithOutHash() error {
-	if len(tw.tableName) > 0 {
-		_, err := fmt.Fprintf(tw.Writer, "; TableName: %s\n", tw.tableName)
+func (w *Writer) writeExtraWithOutHash(d Definition) error {
+	if len(d.tableName) > 0 {
+		_, err := fmt.Fprintf(w.Writer, "; TableName: %s\n", d.tableName)
 		if err != nil {
 			return err
 		}
 	}
-	for key, extra := range tw.Ext {
+	for key, extra := range d.Ext {
 		if extra.hashing {
 			continue
 		}
-		_, err := fmt.Fprintf(tw.Writer, "; %s: %s\n", key, extra.value)
+		_, err := fmt.Fprintf(w.Writer, "; %s: %s\n", key, extra.value)
 		if err != nil {
 			return err
 		}
@@ -73,32 +71,32 @@ func (tw *Writer) writeExtraWithOutHash() error {
 	return nil
 }
 
-func (tw *Writer) writeExtraWithHash() error {
-	if len(tw.Names) > 0 {
-		_, err := io.WriteString(tw.Writer, "; name: ")
+func (w *Writer) writeExtraWithHash(d Definition) error {
+	if len(d.Names) > 0 {
+		_, err := io.WriteString(w.Writer, "; name: ")
 		if err != nil {
 			return err
 		}
-		err = tw.WriteRow(tw.Names)
-		if err != nil {
-			return err
-		}
-	}
-	if len(tw.Types) > 0 {
-		_, err := io.WriteString(tw.Writer, "; type: ")
-		if err != nil {
-			return err
-		}
-		err = tw.WriteRow(tw.Types)
+		err = w.WriteRow(d.Names)
 		if err != nil {
 			return err
 		}
 	}
-	for key, extra := range tw.Ext {
+	if len(d.Types) > 0 {
+		_, err := io.WriteString(w.Writer, "; type: ")
+		if err != nil {
+			return err
+		}
+		err = w.WriteRow(d.Types)
+		if err != nil {
+			return err
+		}
+	}
+	for key, extra := range d.Ext {
 		if !extra.hashing {
 			continue
 		}
-		_, err := fmt.Fprintf(tw.Writer, "; %s: %s\n", key, extra.value)
+		_, err := fmt.Fprintf(w.Writer, "; %s: %s\n", key, extra.value)
 		if err != nil {
 			return err
 		}
@@ -107,8 +105,8 @@ func (tw *Writer) writeExtraWithHash() error {
 }
 
 // WriteRow is write one row.
-func (tw *Writer) WriteRow(row []string) error {
-	_, err := io.WriteString(tw.Writer, "|")
+func (w *Writer) WriteRow(row []string) error {
+	_, err := io.WriteString(w.Writer, "|")
 	if err != nil {
 		return err
 	}
@@ -116,12 +114,12 @@ func (tw *Writer) WriteRow(row []string) error {
 		if strings.Contains(column, "|") {
 			column = ESCREP.ReplaceAllString(column, "|$1")
 		}
-		_, err := io.WriteString(tw.Writer, " "+column+" |")
+		_, err := io.WriteString(w.Writer, " "+column+" |")
 		if err != nil {
 			return err
 		}
 	}
-	_, err = io.WriteString(tw.Writer, "\n")
+	_, err = io.WriteString(w.Writer, "\n")
 	if err != nil {
 		return err
 	}
@@ -130,13 +128,13 @@ func (tw *Writer) WriteRow(row []string) error {
 
 // WriteAll write all table.
 func WriteAll(writer io.Writer, table *Table) error {
-	tw := NewWriter(writer, table.Definition)
-	err := tw.WriteDefinition()
+	w := NewWriter(writer)
+	err := w.WriteDefinition(table.Definition)
 	if err != nil {
 		return err
 	}
 	for _, row := range table.Rows {
-		err = tw.WriteRow(row)
+		err = w.WriteRow(row)
 		if err != nil {
 			return err
 		}
@@ -146,36 +144,41 @@ func WriteAll(writer io.Writer, table *Table) error {
 
 // WriteHashAll write hash ...
 func WriteHashAll(writer io.Writer, table *Table) error {
-	tw := NewWriter(writer, table.Definition)
-	err := tw.writeExtraWithOutHash()
+	w := NewWriter(writer)
+	err := w.writeExtraWithOutHash(table.Definition)
 	if err != nil {
 		return err
 	}
-	tmp := tw.Writer
-
-	var b bytes.Buffer
-	w := bufio.NewWriter(&b)
-	tw.Writer = w
-	err = tw.writeExtraWithHash()
+	b, err := targetBytes(table)
 	if err != nil {
 		return err
 	}
-	for _, row := range table.Rows {
-		err = tw.WriteRow(row)
-		if err != nil {
-			return err
-		}
-	}
-	w.Flush()
-	tw.Writer = tmp
-	sum := sha256.Sum256(b.Bytes())
-	_, err = fmt.Fprintf(tw.Writer, "; sha256: %x\n", sum)
+	sum := sha256.Sum256(b)
+	_, err = fmt.Fprintf(w.Writer, "; sha256: %x\n", sum)
 	if err != nil {
 		return err
 	}
-	_, err = fmt.Fprintf(tw.Writer, "%s\n", b.String())
+	_, err = w.Writer.Write(b)
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func targetBytes(table *Table) ([]byte, error) {
+	var b bytes.Buffer
+	writer := bufio.NewWriter(&b)
+	bw := NewWriter(writer)
+	err := bw.writeExtraWithHash(table.Definition)
+	if err != nil {
+		return nil, err
+	}
+	for _, row := range table.Rows {
+		err = bw.WriteRow(row)
+		if err != nil {
+			return nil, err
+		}
+	}
+	writer.Flush()
+	return b.Bytes(), nil
 }
--- writer_test.go
+++ writer_test.go
@@ -2,60 +2,8 @@ package tbln
 
 import (
 	"bufio"
-	"bytes"
-	"reflect"
 	"testing"
 )
-
-func TestNewWriter(t *testing.T) {
-	type args struct {
-		tbl Definition
-	}
-	tests := []struct {
-		name       string
-		args       args
-		want       *Writer
-		wantWriter string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			writer := &bytes.Buffer{}
-			if got := NewWriter(writer, tt.args.tbl); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewWriter() = %v, want %v", got, tt.want)
-			}
-			if gotWriter := writer.String(); gotWriter != tt.wantWriter {
-				t.Errorf("NewWriter() = %v, want %v", gotWriter, tt.wantWriter)
-			}
-		})
-	}
-}
-
-func TestWriter_WriteDefinition(t *testing.T) {
-	type fields struct {
-		Definition Definition
-		Writer     *bufio.Writer
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tw := &Writer{
-				Definition: tt.fields.Definition,
-				Writer:     tt.fields.Writer,
-			}
-			if err := tw.WriteDefinition(); (err != nil) != tt.wantErr {
-				t.Errorf("Writer.WriteDefinition() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
 
 func TestWriter_WriteRow(t *testing.T) {
 	type fields struct {
@@ -76,8 +24,7 @@ func TestWriter_WriteRow(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			tw := &Writer{
-				Definition: tt.fields.Definition,
-				Writer:     tt.fields.Writer,
+				Writer: tt.fields.Writer,
 			}
 			if err := tw.WriteRow(tt.args.row); (err != nil) != tt.wantErr {
 				t.Errorf("Writer.WriteRow() error = %v, wantErr %v", err, tt.wantErr)
