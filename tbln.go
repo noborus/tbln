3f58ebce86ef4d72534553e0767cb2f957e6eac0
Noboru Saito <noborusai@gmail.com>
2019-02-25 14:08:42 +0900

    update

--- tbln.go
+++ tbln.go
@@ -57,6 +57,20 @@ func NewDefinition() Definition {
 	}
 }
 
+// Extra is table definition extra struct.
+type Extra struct {
+	value      interface{}
+	hashTarget bool
+}
+
+// NewExtra is return new extra struct.
+func NewExtra(value interface{}) Extra {
+	return Extra{
+		value:      value,
+		hashTarget: false,
+	}
+}
+
 // Read is TBLN Read interface.
 type Read interface {
 	ReadRow() ([]string, error)
