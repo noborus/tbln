564dc3de5b98599120c4076ca247daf3cf650805
Noboru Saito <noborusai@gmail.com>
2019-03-30 01:04:05 +0900

    MySQL 5.7 compatible.
    Change to not use the with clause.

--- db/mysql/mysql.go
+++ db/mysql/mysql.go
@@ -58,29 +58,28 @@ func (c *Constr) GetColumnInfo(conn *sql.DB, schema string, tableName string) (m
 			return nil, err
 		}
 	}
-	query := `WITH u AS (
-		SELECT DISTINCT tc.table_name, cc.column_name, 'YES' as constraint_unique
-			FROM information_schema.table_constraints AS tc
-			LEFT JOIN information_schema.KEY_COLUMN_USAGE AS cc
-				ON (tc.table_name = cc.table_name AND tc.constraint_type = 'UNIQUE')
-		 WHERE tc.table_schema = ?
-		 AND tc.table_name = ?
-	)
-	   SELECT column_default
-			  , is_nullable
-			  , data_type AS mysql_type
-			  , character_maximum_length
-			  , character_octet_length
-			  , numeric_precision
-			  , numeric_scale
-			  , datetime_precision
-			  , u.constraint_unique
-			FROM information_schema.columns AS tc
-			LEFT JOIN u
-				     ON (tc.table_name = u.table_name
-				    AND tc.column_name = u.column_name )
-		WHERE tc.table_schema = ?
-	      AND tc.table_name = ?
-		 ORDER BY ordinal_position;`
-	return db.GetColumnInfo(conn, query, schema, tableName, schema, tableName)
+	query := `SELECT cl.column_default
+     , cl.is_nullable
+	 , cl.data_type AS mysql_type
+	 , cl.character_maximum_length
+	 , cl.character_octet_length
+	 , cl.numeric_precision
+	 , cl.numeric_scale
+	 , cl.datetime_precision
+     , (CASE WHEN tc.constraint_type = 'PRIMARY KEY' THEN 'YES'
+             WHEN tc.constraint_type = 'UNIQUE' THEN 'YES'
+         END) AS is_unique
+  FROM information_schema.columns AS cl
+  LEFT JOIN information_schema.key_column_usage AS us
+         ON (cl.table_schema = us.table_schema
+            AND cl.table_name = us.table_name
+            AND cl.column_name = us.column_name)
+       LEFT JOIN information_schema.table_constraints AS tc
+              ON (us.table_name = tc.table_name
+                  AND us.constraint_name = tc.constraint_name
+				  AND us.table_schema = tc.table_schema)
+	WHERE cl.table_schema = ?
+	  AND cl.table_name = ?
+	ORDER BY cl.ordinal_position;`
+	return db.GetColumnInfo(conn, query, schema, tableName)
 }
